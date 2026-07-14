d7bd206bf1112991638c407d51aa3c1d7f658ab9
Glenn Luna <glenncotamuraluna@gmail.com>
2026-07-13 23:00:18 -0600

    Add about page to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -19,6 +19,12 @@ export default function sitemap() {
       priority: 0.9,
     },
     {
+      url: `${siteUrl}/about`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: `${siteUrl}/designer`,
       lastModified,
       changeFrequency: "monthly",
